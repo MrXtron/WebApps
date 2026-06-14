ddc74156635c48724ce93b4dedd4b72d98a05669
MrXtron <srb0123-git@yahoo.com>
2026-06-14 15:17:23 +0530

    Update sw.js

--- Calculator/sw.js
+++ Calculator/sw.js
@@ -1,23 +1,21 @@
-const CACHE_NAME = 'calc-cache-v1';
-const ASSETS = [
-  '/',
-  '/index.html',
-  '/manifest.json'
-];
-
-self.addEventListener('install', (e) => {
-  self.skipWaiting();
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
-  );
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open('v1').then(function(cache) {
+            return cache.addAll([
+                'https://raw.githubusercontent.com/MrXtron/WebApps/refs/heads/main/Calculator/',
+                'https://raw.githubusercontent.com/MrXtron/WebApps/refs/heads/main/Calculator/index.html',
+                'https://raw.githubusercontent.com/MrXtron/WebApps/refs/heads/main/Calculator/manifest.json',
+                'https://raw.githubusercontent.com/MrXtron/WebApps/refs/heads/main/Calculator/icons/icon-192x192.png',
+                'https://raw.githubusercontent.com/MrXtron/WebApps/refs/heads/main/Calculator/icons/icon-512x512.png'
+            ]);
+        })
+    );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request).catch(() => new Response('Offline Mode'));
-    })
-  );
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
 });
