05af93b0f8e479287c7b84ed2b36cd2abb1e360b
MrXtron <275249936+MrXtron@users.noreply.github.com>
2026-08-31 10:27:22 +0530

    Update app.js

--- Calculator/app.js
+++ Calculator/app.js
@@ -1,7 +1,7 @@
 
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
-        navigator.serviceWorker.register('https://raw.githubusercontent.com/MrXtron/WebApps/refs/heads/main/Calculator/sw.js').then(function(registration) {
+        navigator.serviceWorker.register('sw.js').then(function(registration) {
             console.log('ServiceWorker registration successful with scope: ', registration.scope);
         }, function(error) {
             console.log('ServiceWorker registration failed: ', error);
